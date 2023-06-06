d5d5c1f987c73173793a91e82447aa7393707402
israelsilvaaa <Israel524.is@gmail.com>
2023-06-06 11:03:02 -0300

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   app/Http/Controllers/PhotoController.php
    	modified:   routes/web.php

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Staff;
+use App\Models\Photo;
+use App\Models\Product;
 
 class PhotoController extends Controller
 {
-    //
+    public function createProductPhoto(Request $request)
+    {
+        Product::findOrFail($request->id)->photos()->create(['path' => $request->path]);
+        return redirect('/product/' . $request->id . '/photos/read/all');
+    }
+
+    public function createStaffPhoto(Request $request)
+    {
+        Staff::findOrFail($request->id)->photos()->create(['path' => $request->path]);
+        return redirect('/staff/' . $request->id . '/photos/read/all');
+    }
+
+    public function readProductPhoto(Request $request)
+    {
+        foreach (Product::findOrFail($request->id)->photos as $photo) {
+            echo '<br>Id: ' . $photo->id . ', Path: ' . $photo->path . ';';
+        };
+    }
+
+    public function readStaffPhoto(Request $request)
+    {
+        foreach (Staff::findOrFail($request->id)->photos as $photo) {
+            echo '<br>Id: ' . $photo->id . ', Path: ' . $photo->path . ';';
+        };
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Staff;
-use App\Models\Photo;
-use App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,24 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/staff/{id}/photos/create/{path}', function ($id, $path) {
-    Staff::find($id)->photos()->create(['path' => $path]);
-    return redirect('/staff/' . $id . '/photos/read/all');
-});
+Route::get('/staff/{id}/photos/create/{path}', [PhotoController::class, 'createStaffPhoto']);
 
-Route::get('/product/{id}/photos/create/{path}', function ($id, $path) {
-    Product::find($id)->photos()->create(['path' => $path]);
-    return redirect('/product/' . $id . '/photos/read/all');
-});
+Route::get('/product/{id}/photos/create/{path}', [PhotoController::class, 'createProductPhoto']);
 
-Route::get('/staff/{id}/photos/read/all', function ($id) {
-    foreach (Staff::find($id)->photos as $photo) {
-        echo '<br>Id: ' . $photo->id . ', Path: ' . $photo->path . ';';
-    };
-});
+Route::get('/staff/{id}/photos/read/all', [PhotoController::class, 'readStaffPhoto']);
 
-Route::get('/product/{id}/photos/read/all', function ($id) {
-    foreach (Product::find($id)->photos as $photo) {
-        echo '<br>Id: ' . $photo->id . ', Path: ' . $photo->path . ';';
-    };
-});
+Route::get('/product/{id}/photos/read/all', [PhotoController::class, 'readProductPhoto']);
